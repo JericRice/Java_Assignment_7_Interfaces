fe7c01d2a0d790e6761226347663f185c25d51ca
Joe <jeric720@icloud.com>
2021-01-12 21:58:48 -0500

    Initial commit

---
+++ src/one/Automobile.java
@@ -0,0 +1,16 @@
+package one;
+
+
+///**
+// * An Automobile implements the Movable interface
+// *
+// */
+
+public class Automobile implements Movable {
+
+    public void move() {
+
+        System.out.println("Moving the object ... ");
+
+    }
+}
---
+++ src/two/Application2.java
@@ -0,0 +1,14 @@
+package two;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        Animatable animate = new MoverAndAnimate();
+
+        animate.animate();
+        animate.move();
+    }
+
+
+}
